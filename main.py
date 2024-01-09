99c937fedf286524132c900614a6582d5af29ed1
Boncharik <155877706+Boncharik@users.noreply.github.com>
2024-01-09 21:27:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import requests
-from tkinter import Tk, Label, Entry, Button
+from tkinter import Tk, Label, Entry, Button, filedialog
 from urllib.parse import unquote
+import pickle
+import os
+
+filename = ''
+
 
 def download_xlsx():
     url = entry.get()
@@ -20,15 +25,14 @@ def download_xlsx():
         print(f"Ошибка при получении Google Таблицы: {e}")
         return
 
-    # Имя файла - последняя часть ссылки
     table_id = url.split("/")[-2]
-    print(table_id)
 
     download_url = f'https://docs.google.com/spreadsheets/d/{table_id}/export?format=xlsx'
 
     # Отправляем GET-запрос и сохраняем файл
     response = requests.get(download_url)
 
+    global filename
     filename = unquote(response.headers.get('Content-Disposition').split('filename*=UTF-8\'\'')[1]).replace(';', '_')
 
     # Скачивание таблицы и сохранение ее в файл
@@ -36,7 +40,25 @@ def download_xlsx():
     with open(filename, 'wb') as file:
         file.write(response.content)
 
-    print(f"Google Таблица успешно скачана и сохранена в файле: {table_id}")
+    print(f"Google Таблица успешно скачана и сохранена в файле: {filename}")
+
+
+file_path = ''
+
+
+def browse_file():
+    global file_path
+    file_path = filedialog.askopenfilename()
+
+    # Сохранение пути к файлу в файле pickle
+    with open('file_path.pickle', 'wb') as fp:
+        pickle.dump(file_path, fp)
+
+    # Обновление текста Label с выбранным путем и названием файла
+    file_label.config(text=f'Выбран файл: {os.path.basename(file_path)}')
+
+    pass
+
 
 # Создание пользовательского интерфейса
 root = Tk()
@@ -48,43 +70,53 @@ label.pack()
 entry = Entry(root, width=50)
 entry.pack()
 
-button = Button(root, text='Скачать', command=download_xlsx)
-button.pack()
+# Создание Label для отображения выбранного пути и названия файла
+file_label = Label(root, text='')
+file_label.pack()
+
+button_browse = Button(root, text='Обзор', command=browse_file)
+button_browse.pack()
+
+button_download = Button(root, text='Скачать', command=download_xlsx)
+button_download.pack()
+
+# Попытка загрузить путь к файлу из pickle, если он существует
+try:
+    with open('file_path.pickle', 'rb') as f:
+        file_path = pickle.load(f)
+        file_label.config(text=f'Выбран файл: {os.path.basename(file_path)}')
+except FileNotFoundError:
+    file_path = None
 
 root.mainloop()
 
-
 # Считываем файлы в pandas DataFrame
-df_xlsx = pd.read_excel('ИКТК-11 (ТКП).xlsx', header=1)
-df_csv = pd.read_csv('stat_231218_18-22.csv', header=0)
+df_xlsx = pd.read_excel(filename, header=1)
+df_csv = pd.read_csv(file_path, header=0)
 
 # Сравниваем столбцы
 df_xlsx.columns = [col.strip() for col in df_xlsx.columns]
 df_csv.columns = [col.strip() for col in df_csv.columns]
-print(df_xlsx.columns)
-print(df_csv.columns)
 
-# Сравнение столбцов
 common_cols = [col for col in df_xlsx.columns if col in df_csv.columns]
 df_common_cols = df_csv[common_cols]
-print(common_cols)
 
-# Загрузка данных из xlsx
+# Загрузка данных из csv в xlsx
 try:
-    xlsx_data = pd.read_excel('ИКТК-11 (ТКП).xlsx')
-    xlsx_sheets = pd.ExcelFile("ИКТК-11 (ТКП).xlsx").sheet_names
-    # Обновление данных, если вкладка уже существует
+    xlsx_data = pd.read_excel(filename)
+    xlsx_sheets = pd.ExcelFile(filename).sheet_names
+    # Обновление данных, если лист уже существует
 
     if 'SOTSBI' in xlsx_sheets:
-        with pd.ExcelWriter("ИКТК-11 (ТКП).xlsx", mode="a", if_sheet_exists="replace") as writer:
+        with pd.ExcelWriter(filename, mode="a", if_sheet_exists="replace") as writer:
             df_common_cols.to_excel(writer, sheet_name='SOTSBI', index=False, columns=df_csv.columns)
 
-    # Добавление новой вкладки, если вкладка не существует
+    # Добавление нового листа, если лист не существует
     else:
-        with pd.ExcelWriter('ИКТК-11 (ТКП).xlsx', engine='openpyxl', mode='a') as writer:
+        with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
             df_common_cols.to_excel(writer, sheet_name='SOTSBI', index=False)
 except FileNotFoundError:
     # Создание нового файла xlsx, если файл не существует
-    with pd.ExcelWriter('ИКТК-11 (ТКП).xlsx') as writer:
+    with pd.ExcelWriter(filename) as writer:
         df_common_cols.to_excel(writer, sheet_name='SOTSBI', index=False)
-print('Таблица успешно добавлена в файл xlsx')
+print(f'Таблица успешно добавлена в {filename}') в файл xlsx')
